dc960c0066601ad8106093e6a282d82cc067773e
Josh Walters <39789483+joshnwalters@users.noreply.github.com>
2019-02-04 11:42:25 -1000

    completed ziplist

---
+++ ziplist.js
@@ -0,0 +1,23 @@
+function zipItUp (list1, list2) {
+  const list3 = [];
+
+  let i = 0;
+  while (i < list1.length) {
+    list3.push(list1[i]);
+    list3.push(list2[i]);
+    i++;
+
+  }
+  return list3;
+}
+
+function easyZipItUp (list1, list2) {
+  return _.zip(list1, list2);
+}
+
+const list1 = ['a', 'b', 'c'];
+const list2 = [1, 2, 3];
+
+console.log(zipItUp(list1, list2));
+
+console.log(easyZipItUp(list1, list2));
